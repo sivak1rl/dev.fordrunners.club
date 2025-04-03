4ce5845cb2055e4cbd84f2cdebe094f214da0949
Richard Sivak <sivak.r101@gmail.com>
2025-04-03 13:52:11 -0400

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -3,5 +3,5 @@ from frc import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    debug_mode = os.getenv('FLASK_DEBUG', '0') == '1'
+    debug_mode = os.environ.get('FLASK_ENV') == 'development'
     app.run(debug=debug_mode)
